b3e57a85d5647e34b3024cbe05e4378deb3d58a7
Beirun <orillobernardjay@gmail.com>
2025-11-10 21:21:32 +0800

    Update OrdersView

--- src/components/OrdersView.vue
+++ src/components/OrdersView.vue
@@ -10,7 +10,7 @@ import {
 import { Clock, ChefHat, CircleCheckBig, Truck } from 'lucide-vue-next'
 import { Separator } from '@/components/ui/separator'
 import type { Order } from '@/models/order'
-import { computed, onBeforeMount } from 'vue'
+import { computed } from 'vue'
 import { useMenuStore } from '@/stores/menu'
 
 interface Props {
@@ -44,10 +44,10 @@ const total = computed(() => {
   //
   return props.item.details.reduce((sum, item) => {
     const menuItem = menu.items.find((i) => i.item_Id === item.item_Id)
-    console.log('menuItem', menuItem)
     return sum + item.quantity * (menuItem?.price ?? 0)
   }, 9)
 })
+console.log("itemprop", props.item)
 </script>
 
 <template>
@@ -55,25 +55,26 @@ const total = computed(() => {
     <CardHeader>
       <CardTitle class="flex gap-2 justify-between">
         <div class="flex gap-2 items-center">
-          <component :is="statuses[item.status_Id].icon" class="size-5" />
-          <p class="font-semibold">{{ statuses[item.status_Id].name }}</p>
+          <component :is="statuses[item.status_Id! - 1].icon" class="size-5" />
+          <p class="font-semibold">{{ statuses[item.status_Id! - 1].name }}</p>
         </div>
         <p class="font-bold">₱{{ total }}</p>
       </CardTitle>
-      <CardDescription>Order placed on Agust 23, 2025 7:02 AM</CardDescription>
+      <CardDescription>Order placed on {{ new Date(item.created_At!).toLocaleString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
+        }) }}</CardDescription>
     </CardHeader>
     <CardContent class="flex justify-between">
       <div class="text-sm lg:text-md gap-3 w-full flex flex-col rounded-2xl">
         <div class="w-full px-0 sm:px-0 md:px-5 flex justify-between">
-          <div
-            v-for="status in statuses"
-            key="status.id"
-            class="flex flex-col justify-center items-center"
-          >
-            <Button
-              class="cursor-pointer p-2 rounded-full shadow-[-6px_-6px_12px_#ffffff,6px_6px_12px_#BEBEBE]"
-            >
-              <component :is="status.icon" class="size-5 text-[#FF6B6B]" />
+          <div v-for="status in statuses" :key="status.id" class="flex flex-col justify-center items-center">
+            <Button class=" p-2 rounded-full shadow-[-6px_-6px_12px_#ffffff,6px_6px_12px_#BEBEBE]">
+              <component :is="status.icon" class="size-5" :class="status.id === item.status_Id ? 'text-[#FF6B6B]': 'text-gray-600'" />
             </Button>
             <p>{{ status.name }}</p>
           </div>
@@ -82,20 +83,17 @@ const total = computed(() => {
         <p>Order Items:</p>
 
         <!-- to be looped for items -->
-        <div v-for="item in item.details" class="w-full flex gap-3 justify-between">
+        <div v-for="(item, index) in item.details" :key="index" class="w-full flex gap-3 justify-between">
           <p>
-            {{ menu.items.find((i) => i.item_Id === item.item_Id)?.name }} x {{ item.quantity }}
+            {{menu.items.find((i) => i.item_Id === item.item_Id)?.name}} x {{ item.quantity }}
           </p>
-          <p>₱{{ item.quantity * menu.items.find((i) => i.item_Id === item.item_Id)?.price }}</p>
+          <p>₱{{item.quantity * (menu.items.find((i) => i.item_Id === item.item_Id)?.price ?? 0)}}</p>
         </div>
 
         <p class="font-semibold">Special Instructions:</p>
-        <Textarea
-          disabled
-          :value="item.instruction"
+        <Textarea disabled :value="item.instruction"
           class="rounded-md bg-[#F8FAFC] p-3 w-full text-sm resize-none border-none focus:border-none focus:outline-none focus-visible:ring-0 shadow-[inset_0px_0px_0px_#ffffff,inset_3px_3px_5px_#BEBEBE]"
-          placeholder="No special instructions."
-        ></Textarea>
+          placeholder="No special instructions."></Textarea>
       </div>
     </CardContent>
     <CardFooter class="flex justify-center text-xs text-[#6B7280]">
